f48340a170da8c793c674fdf2801980987c79071
Annie <annieharamatcal@gmail.com>
2018-08-15 14:26:38 +0300

    Changed sources file to be a template, added expected result for each test
    Can either be expected versions content, or expected error type

--- test_combo.py
+++ test_combo.py
@@ -1,8 +1,64 @@
 import sys
 sys.path.append('C:/Combo/combo')
 import os
+import json
 
-from dependencies_manager import *
+from dependencies_manager import DependenciesManager
+from combo_general import ComboException
+from source_locator_server import get_version_source
+
+
+ROOT_DIR_NAME = 'root'
+SOURCES_TEMPLATE_FILE_NAME = 'sources_template.json'
+SOURCES_FILE_NAME = 'local_sources.json'
+EXPECTATION_FILE_NAME = 'expected.json'
+
+
+def normalize_sources_json(sub_dir, in_file_name, out_file_name):
+    replacement = os.path.abspath(sub_dir)
+    with open(os.path.join(sub_dir, in_file_name), 'r') as in_file:
+        template_data = in_file.read()
+
+    modified_data = template_data.replace('...', replacement).replace('\\', '/')
+    with open(os.path.join(sub_dir, out_file_name), 'w') as out_file:
+        out_file.write(modified_data)
+
+
+def cmptree(s1, s2):
+    return True  # TODO: Implement
+
+
+def confirm_dependency_version(manager, expected_dependency, sources_file):
+    # TODO: Fix exceptions
+    expected_import_path = manager.get_dependency_path(expected_dependency['name'])
+
+    if not os.path.exists(expected_import_path):
+        raise BaseException()
+
+    import_src_path = get_version_source(expected_dependency['name'], expected_dependency['version'], sources_file)
+    if not cmptree(expected_import_path, import_src_path):
+        raise BaseException()
+
+
+def run_test(root_dir, sources_file, expected_result):
+    expected_error = expected_result.get('error')
+
+    try:
+        manager = DependenciesManager(root_dir, sources_file)
+        manager.resolve()
+
+        if expected_error:
+            raise RuntimeError('Error {} was expected, but did not occur'.format(expected_error))
+
+        expected_dependencies = expected_result['imports']
+
+    except ComboException as e:
+        if type(e).__name__ != expected_error:
+            raise e
+        return
+
+    for expected_dependency in expected_dependencies:
+        confirm_dependency_version(manager, expected_dependency, sources_file)
 
 
 def main():
@@ -15,15 +71,20 @@ def main():
             return False
         return True
 
-    test_sub_dirs = [os.path.join(my_dir, name) for name in os.listdir(my_dir) if is_test_case_dir(name)]
+    test_sub_dirs = [name for name in os.listdir(my_dir) if is_test_case_dir(name)]
 
     for sub_directory in test_sub_dirs:
-        print('Testing directory {}...\n'.format(sub_directory))
+        print('Testing directory "{}"...'.format(sub_directory))
 
-        sources_file = os.path.join(sub_directory, 'sources.json')
-        root_dir = os.path.join(sub_directory, 'root')
+        with open(os.path.join(sub_directory, EXPECTATION_FILE_NAME), 'r') as expected_file:
+            expected_values = json.load(expected_file)
 
-        DependenciesManager(root_dir, sources_file).resolve()
+        normalize_sources_json(sub_directory, SOURCES_TEMPLATE_FILE_NAME, SOURCES_FILE_NAME)
+
+        sources_file = os.path.join(sub_directory, SOURCES_FILE_NAME)
+        root_dir = os.path.join(sub_directory, ROOT_DIR_NAME)
+
+        run_test(root_dir, sources_file, expected_values)
 
 
 if __name__ == '__main__':
